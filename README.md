ae647143de52abe5acc7d52da66dc498882b6f3a
ranamutta <sarirana98@gmail.com>
2023-12-26 23:50:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+<h1>Twitter-Project</h1>
+
+
+Özellikler
+
+-Placeholder Rengi Değiştirme:Metin giriş kutusu tıklandığında, placeholder'ın rengi değişir.
+
+-Metin Uzunluğu Kontrolü:Girilen metnin uzunluğu kontrol edilir ve belirlenen sınırlar içinde tutulur.
+
+-Tweet Gönderme Butonu Etkinliği:Girilen metin olumlu şartları sağladığında, tweet gönderme butonu etkinleştirilir.
+
+-Karakter Sayacı:Kullanıcıya kalan karakter sayısı gösterilir.
+
+
+
+<img src=".twitter.gif"/>
---
+++ main.js
@@ -0,0 +1,56 @@
+const placeholder = document.querySelector(".placeholder");
+
+const editableInput = document.querySelector(".editable");
+
+const tweetButton = document.querySelector(".button");
+
+const counter = document.getElementById("counter");
+
+const readonly=document.querySelector(".readonly");
+
+editableInput.addEventListener("click", () => {
+  placeholder.style.color = "#98a5b1";
+});
+
+editableInput.onblur = () => {
+  placeholder.style.color = "#333";
+};
+
+editableInput.onkeypress = (e) => {
+  placeholder.style.display = "none";
+  inputValidate(e.target.innerText);
+};
+
+editableInput.onkeyup = (e) => {
+  placeholder.style.display = "none";
+  inputValidate(e.target.innerText);
+};
+
+const inputValidate = (tweet) => {
+  const tweetLength = tweet.length;
+  const tweetLimit = 5;
+  const currentLimit = tweetLimit - tweetLength;
+
+  if (tweetLength <= 0) {
+    placeholder.style.display = "block";
+    tweetButton.classList.remove("active");
+    counter.style.display = "none";
+  } else {
+    tweetButton.classList.add("active");
+    counter.style.display = "block";
+    counter.innerText = currentLimit;
+  }
+  let newTweet;
+  if (tweetLength > tweetLimit) {
+    let overTweet = tweet.substr(tweetLimit, tweetLength);
+    let overTweetElement = `<span class="overTweet">${overTweet}</span>`;
+    newTweet = tweet.substr(0, tweetLimit) + overTweetElement;
+    readonly.style.zIndex="1"
+    counter.style.color = "red";
+    tweetButton.classList.remove("active");
+  } else {
+    counter.style.color = "#333";
+    readonly.style.zIndex="-5"
+  }
+  readonly.innerHTML=newTweet
+};
